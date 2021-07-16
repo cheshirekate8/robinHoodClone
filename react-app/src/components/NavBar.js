5697389ca02220d2c41264d333160f67eaf8cb7d
Ben <byoo0522@gmail.com>
2021-07-16 16:41:59 -0700

    format navbar alignment

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,37 +1,45 @@
-
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import LogoutButton from './auth/LogoutButton';
+import '../styles/navbar.css'
 
 const NavBar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
-        </li>
-        <li>
-          <LogoutButton />
-        </li>
-      </ul>
-    </nav>
+    <div >
+      <nav className='navbar-container'>
+        <ul>
+
+          <li className='navbar-link'>
+            <NavLink to='/' exact={true} activeClassName='active'>
+              Home
+            </NavLink>
+          </li>
+
+          <li className='navbar-link'>
+            <NavLink to='/login' exact={true} activeClassName='active'>
+              Login
+            </NavLink>
+          </li>
+
+          <li className='navbar-link'>
+            <NavLink to='/sign-up' exact={true} activeClassName='active'>
+              Sign Up
+            </NavLink>
+          </li>
+
+          <li className='navbar-link'>
+            <NavLink to='/users' exact={true} activeClassName='active'>
+              Users
+            </NavLink>
+          </li>
+
+          <li className='navbar-link'>
+            <LogoutButton />
+          </li>
+
+        </ul>
+      </nav >
+    </div >
   );
 }
 
