393398ae8b509a3e61b4bd4406ecbedaa114c1f5
J0914 <jordynsechrist@gmail.com>
2021-07-19 13:07:44 -0500

    initial setup for stock store

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,8 +4,14 @@ import { Provider } from 'react-redux';
 import './index.css';
 import App from './App';
 import configureStore from './store';
+import * as stockActions from './store/stock';
 
 const store = configureStore();
+if (process.env.NODE_ENV !== 'production') {
+  
+  window.store = store;
+  window.stockActions = stockActions;
+}
 
 ReactDOM.render(
   <React.StrictMode>
--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
-import session from './session'
+import sessionReducer from './session'
+import stockReducer from './stock'
 
 const rootReducer = combineReducers({
-  session,
+  session: sessionReducer, stock: stockReducer,
 });
 
 
