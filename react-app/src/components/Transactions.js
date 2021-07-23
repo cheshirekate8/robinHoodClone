6316f8e1eaafacee1f969c77540e63f877730633
Ben <byoo0522@gmail.com>
2021-07-23 09:53:23 -0700

    fix usersList error

--- react-app/src/components/Transactions.js
+++ react-app/src/components/Transactions.js
@@ -5,14 +5,14 @@ import { NavLink } from 'react-router-dom';
 function Transactions() {
   const [users, setUsers] = useState([]);
 
-  useEffect(() => {
-    async function fetchData() {
-      const response = await fetch('/api/transactions/');
-      const responseData = await response.json();
-      setUsers(responseData.users);
-    }
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   async function fetchData() {
+  //     const response = await fetch('/api/transactions/');
+  //     const responseData = await response.json();
+  //     setUsers(responseData.users);
+  //   }
+  //   fetchData();
+  // }, []);
 
   // const userComponents = users.map((user) => {
   //   return (
