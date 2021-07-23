0b494ac1772093a025adb453f63d852c7fea2243
Katie Young <Katie.f.Young@me.com>
2021-07-23 18:43:51 -0400

    Asset page half styled, needs form and font styled

--- react-app/src/components/AssetPage.js
+++ react-app/src/components/AssetPage.js
@@ -33,7 +33,7 @@ function AssetPage() {
             let total = price * shares
             let newBalance = currentUser.balance - total
             console.log(newBalance)
-            dispatch(transActions.postTransactions({userId:currentUser.id, symbol:currentStock.symbol, shares:shares, total:total, balance:newBalance, buy:'yes', sell:null}))
+            dispatch(transActions.postTransactions({ userId: currentUser.id, symbol: currentStock.symbol, shares: shares, total: total, balance: newBalance, buy: 'yes', sell: null }))
             //Add validator where if newBalance < 0 return error
             history.push(`/transactions`)
         }
@@ -41,7 +41,7 @@ function AssetPage() {
             let total = price * shares
             let newBalance = currentUser.balance + total
             console.log(newBalance)
-            dispatch(transActions.postTransactions({userId:currentUser.id, symbol:currentStock.symbol, shares:shares, total:total, balance:newBalance, buy: null, sell:'yes'}))
+            dispatch(transActions.postTransactions({ userId: currentUser.id, symbol: currentStock.symbol, shares: shares, total: total, balance: newBalance, buy: null, sell: 'yes' }))
             history.push(`/transactions`)
         }
     }
@@ -55,24 +55,25 @@ function AssetPage() {
     } else {
         return (
             <div className='stockDiv'>
-                <div>
-                    <h1>{currentStock?.name} ({currentStock?.symbol})</h1>
-                    {/* <div> <AssetLineGraph /> </div> */}
-                    {/* <img max-width="500px" src={currentStock.imgUrl} /> */}
-                    <div>{currentStock?.ceo}</div>
-                    <div>{currentStock?.employees}</div>
-                    <div>{currentStock?.headquarters}</div>
-                    <div>{currentStock?.founded}</div>
-                </div>
-                <div className='stockButtonsDiv'>
-                    <form
-                        className='transactionForm'
-                        onSubmit={handleSubmit}>
-                        <input placeholder="How much stock to buy?" type="number" step="1" onChange={(e)=>setShares(e.target.value)}></input>
-                        <button value='buy' onClick={() => btnVal = 'BUY'}>Buy</button>
-                        <button value='sell' onClick={() => btnVal = 'SELL'}>Sell</button>
-                    </form>
-                </div>
+                <h1 className='stockTitle'>{currentStock?.name} ({currentStock?.symbol})</h1>
+                <div className='lineGraphDiv'> <AssetLineGraph /> </div>
+                {/* <img className='imgDiv' src={currentStock.imgUrl} /> */}
+                <div className='ceoLabel'>CEO</div>
+                <div className='ceoInfo'>{currentStock?.ceo}</div>
+                <div className='employeedLabel'>Employees</div>
+                <div className='employeesInfo'>{currentStock?.employees}</div>
+                <div className='headquartersLabel'>Headquarters</div>
+                <div className='headquartersInfo'>{currentStock?.headquarters}</div>
+                <div className='foundedLabel'>Founded</div>
+                <div className='foundedInfo'>{currentStock?.founded}</div>
+                <form
+                    className='transactionForm'
+                    onSubmit={handleSubmit}>
+                    <div for='stockInput'> Buy or Sell ({currentStock?.symbol})!</div>
+                    <input name="stockInput" placeholder="Please enter an amount" type="number" step="1" onChange={(e) => setShares(e.target.value)}></input>
+                    <button value='buy' onClick={() => btnVal = 'BUY'}>Buy</button>
+                    <button value='sell' onClick={() => btnVal = 'SELL'}>Sell</button>
+                </form>
             </div>
         )
     }
